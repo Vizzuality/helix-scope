c701279698b6b64402e2ece787b3cd2afa026443
j8seangel <jseangel@gmail.com>
2016-09-07 19:03:36 +0200

    Remove console.log

--- config/environments/production.js
+++ config/environments/production.js
@@ -4,7 +4,6 @@ const express = require('express');
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
 module.exports = function (app) {
-  console.log('pro');
   app.use(express.static(path.join(process.cwd(), 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(indexPath);
