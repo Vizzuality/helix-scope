2c3cf6d7b167fa902014f296cac4dcd023ca0fca
j8seangel <jseangel@gmail.com>
2016-09-13 12:52:57 +0200

    Fix props param

--- app/components/common/Header.js
+++ app/components/common/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import NavLink from 'components/common/NavLink';
 import Button from 'components/common/Button';
 
-const Header = () => (
+const Header = (props) => (
   <div>
     <header>
       <nav className="c-header">
-        <NavLink to="/"><img src="/images/Helixlogo01.png" className="icon-logo"/></NavLink>
+        <NavLink to="/"><img alt="Helix Scope" src="/images/Helixlogo01.png" className="icon-logo" /></NavLink>
         <ul className="main-menu">
           <li>
             <NavLink to="/global-scenarios/19.15/-60.91/3?maps=1,climate,avg-precipitation" className="-green">Global Scenarios</NavLink>
@@ -18,12 +18,12 @@ const Header = () => (
             <NavLink to="/compare" className="-red">Compare</NavLink>
           </li>
           <li>
-            <Button icon="share" style="none" size="small" onClick={() => props.setShareModal(true)}/>
+            <Button icon="share" style="none" size="small" onClick={() => props.setShareModal(true)} />
           </li>
         </ul>
         <ul className="mobile-menu">
           <li>
-            <Button icon="menu" style="none" size="small" onClick={() => props.setMenuModal(true)}/>
+            <Button icon="menu" style="none" size="small" onClick={() => props.setMenuModal(true)} />
           </li>
         </ul>
       </nav>
