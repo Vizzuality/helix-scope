7a12035c1b292601b568ac6b6165912abb65d3f3
j8seangel <jseangel@gmail.com>
2016-09-29 12:31:18 +0200

    Get countries list from carto

--- app/actions/countries.js
+++ app/actions/countries.js
@@ -1,6 +1,21 @@
 import { push } from 'react-router-redux';
 
 export const GET_COUNTRY_DATA = 'GET_COUNTRY_DATA';
+export const GET_COUNTRIES_LIST = 'GET_COUNTRIES_LIST';
+
+export function getCountriesList() {
+  const url = 'https://helixscope.carto.com/api/v2/sql?q=SELECT%20name_engli%20as%20name,%20iso%20FROM%20country_geoms%20ORDER%20BY%20name%20ASC';
+  return dispatch => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        dispatch({
+          type: GET_COUNTRIES_LIST,
+          payload: data.rows
+        });
+      });
+  };
+}
 
 export function getCountryData(slug) {
   return {
--- app/reducers/countries.js
+++ app/reducers/countries.js
@@ -1,7 +1,13 @@
-import { GET_COUNTRY_DATA } from 'actions/countries';
+import { GET_COUNTRIES_LIST, GET_COUNTRY_DATA } from 'actions/countries';
 
-export default function (state = {}, action) {
+const initialState = {
+  countriesList: []
+};
+
+export default function (state = initialState, action) {
   switch (action.type) {
+    case GET_COUNTRIES_LIST:
+      return Object.assign({}, state, { countriesList: action.payload });
     case GET_COUNTRY_DATA:
       return Object.assign({}, state, action.payload);
     default:
