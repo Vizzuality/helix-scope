3ee9073ebb5e4414e33ade465ebb9562891c0a0b
j8seangel <jseangel@gmail.com>
2016-09-29 15:09:42 +0200

    Use endpoint constant

--- app/actions/countries.js
+++ app/actions/countries.js
@@ -1,10 +1,11 @@
 import { push } from 'react-router-redux';
+import { ENDPOINT_SQL } from 'constants/map';
 
 export const GET_COUNTRY_DATA = 'GET_COUNTRY_DATA';
 export const GET_COUNTRIES_LIST = 'GET_COUNTRIES_LIST';
 
 export function getCountriesList() {
-  const url = 'https://helixscope.carto.com/api/v2/sql?q=SELECT%20name_engli%20as%20name,%20iso%20FROM%20country_geoms%20ORDER%20BY%20name%20ASC';
+  const url = `${ENDPOINT_SQL}?q=SELECT%20name_engli%20as%20name,%20iso%20FROM%20country_geoms%20ORDER%20BY%20name%20ASC`;
   return dispatch => {
     fetch(url)
       .then(response => response.json())
