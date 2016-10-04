292e33d6b8572cad41fa579c5b1c4fd2ca00b1c9
j8seangel <jseangel@gmail.com>
2016-10-04 15:41:23 +0200

    Remove old breadcrumb

--- app/components/pages/ComparePage.js
+++ app/components/pages/ComparePage.js
@@ -10,7 +10,6 @@ function ComparePage() {
       <div className="l-banner -compare">
         <div className="row">
           <div className="column">
-            <div className="c-breadcrumbs -inv">Home / Compare</div>
             <div className="c-txt-title -inv">Compare</div>
             <div className="c-txt-intro -inv">Climate change will affect different countries in different ways. Choose the countries you are interested in and find out the impact of the 2°C, 4°C and 6°C scenarios.</div>
           </div>
