ba2ba62fcba9c4d394f988ce27e6f390e428892f
Gerardo <gerardo.pacheco@vizzuality.com>
2016-10-05 17:54:41 +0200

    Feat: Map - Invalidate size on adding / deleting maps

--- app/components/maps/MapsList.js
+++ app/components/maps/MapsList.js
@@ -2,32 +2,49 @@ import React from 'react';
 import Map from 'containers/maps/Map';
 import Dashboard from 'containers/maps/DashboardContainer';
 
-function MapsList(props) {
-  const numMaps = props.maps.length;
-  const mapClasses = [
-    ['-full'],
-    ['-horizontal', '-horizontal'],
-    ['-wide', '-wide', '-narrow'],
-    ['-quarter', '-quarter', '-quarter', '-quarter']
-  ];
+class MapsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.invalidateSize = false;
+  }
 
-  return (
-    <div className="l-maps-container">
-      {props.maps.map((map, index) =>
-        <div className={`c-maps-list ${mapClasses[numMaps - 1][index]}`} key={`map-${index}`}>
-          <div className="scenario-wrapper">
-            <Dashboard
-              mapData={map}
-              handleMapConfig={props.handleMapConfig}
-            />
-            <Map
-              mapData={map}
-            />
+  componentWillReceiveProps(props) {
+    if (props.maps && this.props.maps.length !== props.maps.length) {
+      this.invalidateSize = true;
+    } else {
+      this.invalidateSize = false;
+    }
+  }
+
+  render() {
+    const numMaps = this.props.maps.length;
+    const mapClasses = [
+      ['-full'],
+      ['-horizontal', '-horizontal'],
+      ['-wide', '-wide', '-narrow'],
+      ['-quarter', '-quarter', '-quarter', '-quarter']
+    ];
+
+    return (
+      <div className="l-maps-container">
+        {this.props.maps.map((map, index) =>
+          <div className={`c-maps-list ${mapClasses[numMaps - 1][index]}`} key={`map-${index}`}>
+            <div className="scenario-wrapper">
+              <Dashboard
+                mapData={map}
+                handleMapConfig={this.props.handleMapConfig}
+              />
+              <Map
+                mapData={map}
+                invalidateSize={this.invalidateSize}
+              />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
 
 MapsList.propTypes = {
