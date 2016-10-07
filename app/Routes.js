32ed4d4f766b99947bb41fb9236d1f7381dd0319
j8seangel <jseangel@gmail.com>
2016-10-07 17:59:19 +0200

    Add scroll to top on page change

--- app/Routes.js
+++ app/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { IndexRoute, IndexRedirect, Router, Route } from 'react-router';
+import { useScroll } from 'react-router-scroll';
+import { IndexRoute, IndexRedirect, Router, Route, applyRouterMiddleware } from 'react-router';
 import ContainerPage from './containers/pages/ContainerPage';
 import HomePage from './components/pages/HomePage';
 import MapsPage from './containers/pages/MapsPage';
@@ -13,8 +14,79 @@ import AboutPage from './containers/pages/AboutPage';
 import NewsPage from './components/pages/NewsPage';
 import ContactPage from './components/pages/ContactPage';
 
+function shouldUpdateScroll(prevRouterProps, { location }) {
+  /**
+   * Return whether the two pages match the regex and have the same matching
+   * regex parameters
+   * @param  {regex}  regex
+   * @return {Boolean}
+   */
+  function isSamePage(regex) {
+    const pathname = (prevRouterProps && prevRouterProps.location.pathname) || '';
+    const nextPathname = location.pathname;
+
+    /* We first check if the pages are concerned by the regex. If not, the route
+     * isn't matching */
+    const isPathnameConcerned = regex.test(pathname);
+    const isNextPathnameConcerned = regex.test(nextPathname);
+
+    if (!isPathnameConcerned || !isNextPathnameConcerned) {
+      return false;
+    }
+
+    /* We then get the matching regex params and return false if there isn't
+     * any */
+    const routeParams = pathname.match(regex);
+    const nextRouteParams = nextPathname.match(regex);
+
+    if (!routeParams || !nextRouteParams) {
+      return false;
+    }
+
+    /* We remove the first element of the arrays as it is the whole matched
+     * string (i.e. the route) */
+    if (routeParams.length) {
+      routeParams.splice(0, 1);
+    }
+    if (nextRouteParams.length) {
+      nextRouteParams.splice(0, 1);
+    }
+
+    const paramsCount = Math.min(routeParams.length, nextRouteParams.length);
+
+    let doesParamsMatch = true;
+    for (let i = 0, j = paramsCount; i < j; i++) {
+      if (routeParams[i] !== nextRouteParams[i]) {
+        doesParamsMatch = false;
+        break;
+      }
+    }
+
+    return doesParamsMatch;
+  }
+
+  /* Here we define all the routes for which we don't want to scroll to top if
+   * both the old path and the new one match (i.e. if the global regex and the
+   * regex params match the two paths) */
+  const regexes = [
+    /\/dashboard\/((?:[A-z]|[1-9]|-)+)(?:\/(?:.*))?/,
+    /\/insights\/((?:[A-z]|[1-9]|-)+)/
+  ];
+
+  for (let i = 0, j = regexes.length; i < j; i++) {
+    if (isSamePage(regexes[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 const Routes = ({ history }) => (
-  <Router history={history}>
+  <Router
+    history={history}
+    render={applyRouterMiddleware(useScroll(shouldUpdateScroll))}
+  >
     <Route path="/" component={ContainerPage}>
       <IndexRoute component={HomePage} />
       <Route path="maps">
