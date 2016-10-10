bfe1a3d651f99abefbd4e708d0db21d07ff09de3
j8seangel <jseangel@gmail.com>
2016-10-10 11:42:32 +0200

    Update sliders map links and text

--- app/components/home/slides/HomeSlide3.js
+++ app/components/home/slides/HomeSlide3.js
@@ -4,12 +4,12 @@ import TextSlide from './TextSlide';
 function HomeSlide3() {
   return (
     <TextSlide
-      title="Arctic sea ice during winter in the 4ºC world"
-      content="If the global average temperature increases by 4ºC then temperatures in the Arctic could increase by over 8ºC, which means winter sea ice cover may decline by 47%."
+      title="Arctic sea ice during winter in the 2ºC world"
+      content="If the global average temperature increases by 2ºC then temperatures in the Arctic could increase by over 8ºC, which means winter sea ice cover may decline by 47%."
       buttons={[
         {
           text: 'Find out more',
-          link: '/global-scenarios/32.47/-50.10/3?maps=0,climate,avg-precipitation/0,climate,avg-precipitation'
+          link: '/global-scenarios/32.47/-50.10/3?maps=15,climate,avg_temperature_change,mean/2,climate,avg_temperature_change,mean'
         }
       ]}
     />
--- app/components/home/slides/HomeSlide4.js
+++ app/components/home/slides/HomeSlide4.js
@@ -4,12 +4,12 @@ import TextSlide from './TextSlide';
 function HomeSlide4() {
   return (
     <TextSlide
-      title="The 6ºC world"
-      content="If the global temperature increases by 6ºC, more than 60% of the planet species studied become increasingly vulnerable to extinction. If we change our behaviour and limit warming to 2ºC the risk it halved."
+      title="The 4.5ºC world"
+      content="If the global temperature increases by 4.5ºC, more than 60% of the planet species studied become increasingly vulnerable to extinction. If we change our behaviour and limit warming to 2ºC the risk it halved."
       buttons={[
         {
           text: 'Find out more',
-          link: '/global-scenarios/34.05/-88.73/5?maps=0,climate,avg-precipitation/0,climate,avg-precipitation/0,climate,avg-precipitation'
+          link: '/global-scenarios/34.05/-88.73/5?maps=45,climate,avg_temperature_change,max/45,climate,avg_temperature_change,mean/45,climate,avg_temperature_change,min'
         }
       ]}
     />
