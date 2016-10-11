a62bb9d105c364bc94a713a1eefdcb50bfdaab2a
j8seangel <jseangel@gmail.com>
2016-10-11 20:22:49 +0200

    Fix home slide map links

--- app/components/home/slides/HomeSlide4.js
+++ app/components/home/slides/HomeSlide4.js
@@ -5,11 +5,13 @@ function HomeSlide4() {
   return (
     <TextSlide
       title="The 4.5ºC world"
-      content="If the global temperature increases by 4.5ºC, more than 60% of the planet species studied become increasingly vulnerable to extinction. If we change our behaviour and limit warming to 2ºC the risk it halved."
+      content="If the global temperature increases by 4.5ºC, more than 60% of the
+      planet species studied become increasingly vulnerable to extinction. If we
+      change our behaviour and limit warming to 2ºC the risk it halved."
       buttons={[
         {
           text: 'Find out more',
-          link: '/global-scenarios/34.05/-88.73/5?maps=45,climate,avg_temperature_change,max/45,climate,avg_temperature_change,mean/45,climate,avg_temperature_change,min'
+          link: '/global-scenarios/34.05/-88.73/5?maps=45,climate,avg_temperature_change,max,3/45,climate,avg_temperature_change,mean,3/45,climate,avg_temperature_change,min,3'
         }
       ]}
     />
