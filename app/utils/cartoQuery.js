cf7c4ee252b37a1e71db8e072af1380130b4f80b
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-12-07 17:02:59 +0000

    Send queries using post

--- app/utils/cartoQuery.js
+++ app/utils/cartoQuery.js
@@ -1,5 +1,9 @@
 import { ENDPOINT_SQL } from 'constants/map';
 
-const cartoQuery = (sql) => fetch(`${ENDPOINT_SQL}?q=${encodeURIComponent(sql)}`);
+const cartoQuery = (sql) => fetch(`${ENDPOINT_SQL}`, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  body: `q=${encodeURIComponent(sql)}`
+});
 
 export default cartoQuery;
